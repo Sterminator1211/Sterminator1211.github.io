ed6932d01065f94095a9fa253e2e716e6c10e8af
Gamer <erlinerlin888121112@gmail.com>
2026-07-10 19:47:27 -0500

    Remove API key verification from auth.ts
    Removed API key verification logic from verifyAPIKey function.

--- random-stuff/multicode/quick-projects/iods/lib/auth.ts
+++ random-stuff/multicode/quick-projects/iods/lib/auth.ts
@@ -3,17 +3,5 @@ import { IncomingMessage } from "http";
 export function verifyAPIKey(
     req: IncomingMessage
 ): boolean {
-
-    const key =
-        req.headers["x-api-key"];
-
-    if (
-        !key ||
-        typeof key !== "string"
-    ) {
-        return false;
-    }
-
-    return key === process.env.API_KEY;
-
+    return true;
 }
